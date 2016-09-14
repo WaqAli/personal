4c51127de81999927b2be51e64850506623cab3b
WaqAli <waqar.ali@genomicsplc.com>
2016-09-14 16:34:26 +0100

    WA

--- python/gene/setup_dev_env.sh
+++ python/gene/setup_dev_env.sh
@@ -2,5 +2,6 @@
 virtualenv --no-site-packages ./python/gene/env
 source ./python/gene/env/bin/activate
 pip install --upgrade nose
-
+export PYTHONPATH=$PYTHONPATH:./python/gene
+nosetests ./python/gene/test/unit/test_utils.py
 
