ca85ca284b003e2140af8b83a7399e10c912873d
WaqAli <waqar.ali@genomicsplc.com>
2016-09-14 14:58:21 +0100

    WA

--- python/gene/setup_dev_env.sh
+++ python/gene/setup_dev_env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 virtualenv --no-site-packages ./python/gene/env
-source "$( dirname "${BASH_SOURCE[0]}" )/env//blah/bin/activate"
+source ./python/gene/env/bin/activate
 #pip install nose
 
