8ad12128f49e6a238b7818cb8687208ce7194160
WaqAli <waqar.ali@genomicsplc.com>
2016-09-14 16:55:06 +0100

    WA

--- python/gene/setup_dev_env.sh
+++ python/gene/setup_dev_env.sh
@@ -3,5 +3,5 @@ virtualenv --no-site-packages ./python/gene/env
 source ./python/gene/env/bin/activate
 pip install --upgrade nose
 export PYTHONPATH=$PYTHONPATH:./python/gene
-nosetests --with-xunit ./python/gene/test/unit/test_utils.py --xunit-file="/local/system-local/scratch/waqar/zulu/nosetests-unit.xml"
+nosetests --with-xunit ./python/gene/test/unit/test_utils.py --xunit-file="nosetests.xml"
 
