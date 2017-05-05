a2346c635bc707f59e8e0b58faf3897aca0c3938
WaqAli <waqar.ali@genomicsplc.com>
2017-05-05 16:47:24 +0100

    {}

--- spark_course/wikipedia/src/main/scala/wikipedia/WikipediaRanking.scala
+++ spark_course/wikipedia/src/main/scala/wikipedia/WikipediaRanking.scala
@@ -66,7 +66,14 @@ object WikipediaRanking {
    *   Note: this operation is long-running. It can potentially run for
    *   several seconds.
    */
-  def rankLangsUsingIndex(index: RDD[(String, Iterable[WikipediaArticle])]): List[(String, Int)] = ???
+  def rankLangsUsingIndex(index: RDD[(String, Iterable[WikipediaArticle])]): List[(String, Int)] = {
+    
+    index.map(r => (r._1, r._2.size)).collect().toList.sortBy(-_._2)
+    
+    
+    
+    
+  }
 
   /* (3) Use `reduceByKey` so that the computation of the index and the ranking are combined.
    *     Can you notice an improvement in performance compared to measuring *both* the computation of the index
