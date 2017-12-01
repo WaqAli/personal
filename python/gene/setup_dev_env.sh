7a15d40e5a7cb41314de841ab920fab48e1741b1
WaqAli <waqar.ali@genomicsplc.com>
2017-12-01 15:57:09 +0000

    {WA} cleanup

--- python/gene/setup_dev_env.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-virtualenv --no-site-packages ./python/gene/env
-source ./python/gene/env/bin/activate
-pip install --upgrade nose
-export PYTHONPATH=$PYTHONPATH:./python/gene
-nosetests --with-xunit ./python/gene/test/unit/test_utils.py --xunit-file="nosetests.xml"
-
--- python/gene/test/test_gtex_parser.py
+++
@@ -1,7 +0,0 @@
-import unittest
-
-
-class TestGtexParser(unittest.TestCase):
-    def test_should_return_null(self):
-        linearregression()
-        if not linear
